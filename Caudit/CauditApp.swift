a144517e318270188e858590546a1740ef262695
Bengerthelorf <bengerlorf@hotmail.com>
2026-03-09 00:27:27 +0800

    Initial Commit

---
+++ Caudit/CauditApp.swift
@@ -0,0 +1,17 @@
+//
+//  CauditApp.swift
+//  Caudit
+//
+//  Created by Snaix on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CauditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caudit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caudit
+//
+//  Created by Snaix on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
